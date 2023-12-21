ecc0302bb79378c3e6e4e9810cb0d5414840d378
tsioftas <panaevri@gmail.com>
2023-12-21 00:21:05 +0000

    day 4 part 2

--- Day4/d4_2.cpp
+++ Day4/d4_2.cpp
@@ -1,1 +1,24 @@
 #include <d4_common.h>
+
+int main()
+{
+    std::vector<Card> input = read_input();
+    int N = input.size();
+    int multiplier[N];
+    for(int i=0; i < N; ++i)
+    {
+        multiplier[i] = 1;
+    }
+    int cnt = 0;
+    for(int i = 0; i < N; ++i)
+    {
+        cnt += multiplier[i];
+        int w = input[i].how_many_winning();
+        for(int j = 1; j <= w; ++j)
+        {
+            multiplier[i+j] += multiplier[i];
+        }
+    }
+    std::cout << cnt << "\n";
+    return 0;
+}
